2eafa151558fe3f43d031881cd490c6bc6db59d6
Jared Lo <118222769+jlo2224@users.noreply.github.com>
2025-11-10 12:16:34 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // todo
+
+async function callPythonScript() {
+  const response = await fetch('/api.py', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // will fail, just loading with sample code
+    },
+    body: JSON.stringify({ data: 'input' })
+  });
+  const result = await response.json();
+  console.log(result);
+}
